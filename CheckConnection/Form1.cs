f3b71b17b790b18f79c89512268e82d8e15340ee
abdessalam-aadel <abdessalam.aadel@gmail.com>
2019-12-22 12:27:21 +0100

    Firs commit

---
+++ CheckConnection/Form1.cs
@@ -0,0 +1,37 @@
+﻿namespace CheckConnection
+{
+    using System;
+    using System.Windows.Forms;
+    using iTuner;
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ReportAvailability();
+            NetworkStatus.AvailabilityChanged += new NetworkStatusChangedHandler(DoAvailabilityChanged);
+        }
+
+        static void DoAvailabilityChanged(object sender, NetworkStatusChangedArgs e)
+        {
+            ReportAvailability();
+        }
+
+        private static void ReportAvailability()
+        {
+            if (NetworkStatus.IsAvailable)
+            {
+                label1.Text = "... Network is available";
+            }
+            else
+            {
+                label1.Text = "... Network is not available";
+            }
+        }
+    }
+}
